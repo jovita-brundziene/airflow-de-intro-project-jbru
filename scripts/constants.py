9d063f11733e6dfcdf5c9e11abb767c5668c7143
Jovita Brundziene <jovita.brundziene@justice.gov.uk>
2026-02-04 20:31:58 +0000

    repo cleanup

--- scripts/constants.py
+++ scripts/constants.py
@@ -13,3 +13,20 @@ DRY_RUN = False
 RUN_MODE = "write"
 DATA_PATH = "data/input-data"
 OUTPUT_PATH = "data/output-data"
+
+"""
+To do:
+- Outstanding repo steps
+    add mojap columns
+    Write table to S3
+    Move files to raw hist
+    Apply scd2
+- Include dev/prod environment parameters?
+- create a docker image
+- create a github action to run pipeline automatically
+- create unit tests
+- Integration/ end to end testing
+- Update requirements file and build it into the script
+- Requirements lint
+- Nice to have: package it up as a python package?
+"""
